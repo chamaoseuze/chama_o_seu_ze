92d166d3f5c7b69463ae1a8fd9f16bd4bf639b2b
Vinicius Medina <ti@chamaoseuze.com.br>
2024-07-30 20:11:51 -0300

    Atualizações no Site

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const title = document.querySelector('.caixas_titulo');
-    const content = document.querySelector('.caixas_conteudo');
+    const caixas = document.querySelectorAll('.caixas');
 
-    title.addEventListener('click', function() {
-    if (content.style.display === 'none' || content.style.display === '') {
-        content.style.display = 'block'; 
-    } else {
-        content.style.display = 'none'; 
-    }
+    caixas.forEach(caixa => {
+        const title = caixa.querySelector('.caixas_titulo');
+        const content = caixa.querySelector('.caixas_conteudo');
+
+        title.addEventListener('click', function() {
+            caixas.forEach(c => {
+                const otherContent = c.querySelector('.caixas_conteudo');
+                if (otherContent !== content) {
+                    otherContent.style.display = 'none';
+                }
+            });
+
+            if (content.style.display === 'none' || content.style.display === '') {
+                content.style.display = 'block'; 
+            } else {
+                content.style.display = 'none'; 
+            }
+        });
     });
 });
